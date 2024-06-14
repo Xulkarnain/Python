17beceb101661e9e21368a578bd7678d716378ad
Zulkarnain <darzulkarnain@gmail.com>
2024-06-14 13:05:35 +0530

    first commit

---
+++ Lecture 2.py
@@ -0,0 +1,15 @@
+# WAP to input user’s first name & print its length.
+
+Name = str(input("Enter your name = "));
+
+print(len(str(Name)));
+
+# WAP to find the occurrence of ‘$’ in a String.
+
+srt1 = str(input("a = "));
+print(str(srt1));
+
+# WAP to check if a number entered by the user is odd or even.
+
+
+
---
+++ firstprogram.py
@@ -0,0 +1,38 @@
+# print("HELLO WORLD")
+
+# print(23+23)
+
+# print(11*2)
+
+
+#single line comment
+"""multi line 
+comment"""
+
+#Arithmetic operator
+
+# a=5
+# b=2
+# print(a +b)
+# print(a -b)
+# print(a *b)
+# print(a /b)
+# print(a %b)#remainder
+# print(a**b) #apower b
+
+#Assignment operator
+# num = 5
+# num += 5
+# num -= 3
+# num *= 5
+# num /= 5
+# num **= 2
+# print("num =", num) 
+
+#logical operators
+
+a = 40
+b = 30
+
+print(not True)
+print(not (b>a))
