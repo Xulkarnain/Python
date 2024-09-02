d47778449b60fb919e6d0006e929b28e2ffe1de6
Zulkarnain <darzulkarnain@gmail.com>
2024-09-02 07:01:47 +0530

    string

---
+++ Sting.py
@@ -0,0 +1,13 @@
+#Anything enclosed in single or double quotation are called string 
+
+name="zulkarnain"
+name2='hello'
+
+print(name + name2)
+
+#multiline string are enclosed in triple double or single quotation 
+
+loret= """hello
+my name is zulkarnain"""
+
+print(loret)
---
+++ exercise 1.py
@@ -0,0 +1,9 @@
+#make working calculator
+
+a=2
+b=4
+
+print("the divide is = ",a / b)
+print("the sub is = ",a-b)
+print("the multiple is = ",a*b)
+print("the sum is = ",a+b)
